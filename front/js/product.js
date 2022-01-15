cc9ff7b484b71d66a42e3d4cf77ff77f060c143a
Jéssica Azevedo <jessica.azdo@gmail.com>
2022-01-15 15:29:47 +0100

    ajout du projet 5

--- front/js/product.js
+++ front/js/product.js
@@ -20,5 +20,56 @@ let cardsFetch = function () {
         detailsProduits ("description", product.description);
       });
   };
+   //ajoute l'id dans l'url
+   let product=false;
+   for (let i=0; i<product.length; i++){
+    if(product[i]._id === sid){
+       product=product[i];
+    }
+   };
+ // Gestion des éléments dans le local storage 
+  function saveItem(item){
+     localStorage.setItem("item",JSON.stringify(item));
+  }
+// recuperation du panier 
+  function getItem(){
+    let item = localStorage.getItem("item");
+    if(item == null){
+        return [];
+    }else{
+        return JSON.parse(item);
+    }
+    
+  }
+  // trouver produit et gerer sa quantité
+  function addItem (product){
+   let item = getItem();
+   let foundProduct = item.find(prod => prod.id == product.id);
+   if (foundProduct != undefined){
+       foundProduct.quantity++;
+   }else{
+       product.quantity = 1;
+       item.push(product);
+   }
+    
+   saveItem(item);
+  }
+ // Gerer la coulor et la quantité d'un produit 
+  let colorQuantity = document.getElementById("colors");
+  console.log(colorQuantity);
+  console.log(product.colors);
+// à voir 
+    product.colors.forEach((colors) => {
+      console.log(document.createElement("option"));
+      let tagOption = document.createElement("option");
+      tagOption.innerHTML = "vert";
+      tagOption.value = "vert";
+      colorQuantity.appendChild(tagOption);
+  });
+  
+
+
   cardsFetch();
+
+  
 
